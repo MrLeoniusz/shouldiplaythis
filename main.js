bc738ae8feaa449502f25d6c9eb977d703828aff
MrLeoniusz <bernardpawelmarkowski@gmail.com>
2017-08-21 00:21:42 +0200

    function printGame main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 var SteamApi = require('steam-api');
 var key = 'CB5D3B8C8990F4082E3CD42A2BCFF822';
 var app = new SteamApi.App(key);
-app.GetAppList().done(function(result){
+function printGames(word) {
+	app.GetAppList().done(function(result){
 	for (var i=0; i<result.length; i++) {
 	   
-	    if (result[i].name.includes("Portal")) {
+	    if (result[i].name.includes(word)) {
 	    	console.log(result[i].name);
 	    }
 	}
-});
+});}
+	printGames("Strike")
