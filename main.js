85efe71f4f54d7e799dd593525c36c651730e5ff
MrLeoniusz <bernardpawelmarkowski@gmail.com>
2017-08-20 23:50:38 +0200

    main.js

--- main.js
+++ main.js
@@ -2,11 +2,10 @@ var SteamApi = require('steam-api');
 var key = 'CB5D3B8C8990F4082E3CD42A2BCFF822';
 var app = new SteamApi.App(key);
 app.GetAppList().done(function(result){
-  console.log(result);
-});
-app.GetAppList().done(function(result){
 	for (var i=0; i<result.length; i++) {
-	    console.log(result[i]);
+	   
+	    if (result[i].name.includes("Portal")) {
+	    	console.log(result[i].name);
+	    }
 	}
 });
-   
